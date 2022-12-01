2195001b92d59bfa181dd4f5ee7a2daaa795f9d2
FlowerZhou <18840851365@163.com>
2022-11-30 21:31:24 -0500

    run.sh

---
+++ download.sh
@@ -0,0 +1,3 @@
+fileid="1n68Qrf17xJ1dnN_mVQa2QXfDwqhDIARV"
+filename="run.tar.gz"
+curl -L -o ${filename} "https://drive.google.com/uc?export=download&id=${fileid}"
---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=5
+
+labels=reddit,facebook,NeteaseMusic,twitter,qqmail,instagram,weibo,iqiyi,
+labels+=imdb,TED,douban,amazon,youtube,JD,youku,baidu,google,tieba,taobao,bing
+
+
+output_dir=../save/teacher
+
+python run.py \
+    --do_eval \
+    --data_dir ../data \
+    --dataset d1 \
+    --output_dir ${output_dir} \
+    --epochs 5 --labels $labels \
+    --batch_size 256 --gpu 0 --gamma 1 \
+    --model EBSNN_LSTM --segment_len 16 \
+    --embedding_dim 4 \
+    --dropout 0.5 \
+    --no_bidirectional \
+    --log_filename ${output_dir}/plog.log \
+    --logging_steps 50 \
+    --shuffle
