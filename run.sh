454690a59b548489cd860b3643ae398492970e91
FlowerZhou <18840851365@163.com>
2022-12-01 09:31:07 -0500

    run

--- run.sh
+++ run.sh
@@ -9,15 +9,17 @@ labels+=imdb,TED,douban,amazon,youtube,JD,youku,baidu,google,tieba,taobao,bing
 output_dir=../save/teacher
 
 python run.py \
-    --do_eval \
+    --do_train \
     --data_dir ../data \
     --dataset d1 \
     --output_dir ${output_dir} \
-    --epochs 5 --labels $labels \
-    --batch_size 256 --gpu 0 --gamma 1 \
+    --epochs 50 --labels $labels \
+    --batch_size 256 --gpu 0 --gamma 2 \
     --model EBSNN_LSTM --segment_len 16 \
-    --embedding_dim 8 \
+    --embedding_dim 16 \
+    --rnn_dim 16 \
     --dropout 0.5 \
+    --learning_rate 0.01 \
     --no_bidirectional \
     --log_filename ${output_dir}/plog.log \
     --logging_steps 50 \
